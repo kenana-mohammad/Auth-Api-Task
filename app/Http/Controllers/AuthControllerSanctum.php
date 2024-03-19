3644938373c6a4494915bf177c9261cc93eea6aa
kenana-mohammad <kenana4324@gmail.com>
2024-03-19 16:18:31 +0200

    sanctum

---
+++ app/Http/Controllers/AuthControllerSanctum.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use validator;
+use App\Models\user;
+use Illuminate\Http\Request;
+
+use App\Http\Requests\LoginRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\RegisterRequest;
+
+
+
+class AuthControllerSanctum extends Controller
+{
+    //
+    public function register(RegisterRequest $request){
+
+     $validateor =  $request->validated();
+
+
+         $user = user::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+         ]);
+         $token=$user->createToken('MyApp')->plainTextToken;
+         return response()->json([
+            'status' =>true,
+            'user'=> $user,
+            'token' => $token,
+            'msg' =>'User register successfully',
+         ]);
+
+    }
+
+    public function login(LoginRequest $request){
+      $credentials = $request->validated();
+
+          if( Auth::attempt($credentials)){
+            $user =Auth::user();
+            $token=$user->createToken('MyApp')->plainTextToken;
+            return response()->json([
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+                'user' =>$user
+            ]);
+
+             return response()->json([
+                'message' => 'Invalid login details'
+            ], 401);
+        }
+
+    }
+
+    public function logout(){
+        auth()->user()->tokens()->delete();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Successfully logged out',
+
+        ]);
+    }
+}
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,11 +38,22 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
+        // 'api' => [
+        //      \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        //     \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+        //     \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        // ],
         'api' => [
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ],
+
+// 'api' => [
+//     \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+//     'throttle:api',
+//     \Illuminate\Routing\Middleware\SubstituteBindings::class,
+// ],
     ];
 
     /**
